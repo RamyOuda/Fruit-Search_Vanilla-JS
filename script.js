5af050efa149cd1d3869b4bcb601da0252df78af
Ramy Ouda <ramyouda@gmail.com>
2023-10-25 14:36:56 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+import { fruit } from "./fruit.js";
+
+// Access DOM
+const inputElement = document.querySelector("input");
+const ulElement = document.querySelector("ul");
+const suggestionsElement = document.querySelector(".suggestions");
+
+// Event Handlers
+window.onload = resizeSuggestions;
+window.onresize = resizeSuggestions;
+inputElement.onkeyup = updateSuggestions;
+
+// Update LIs in UL
+function updateSuggestions() {
+  clearSuggestions();
+
+  const searchText = inputElement.value;
+
+  if (searchText) {
+    fruit.forEach((fruit) => {
+      const upperSearchText = searchText.toUpperCase();
+      const upperFruit = fruit.toUpperCase();
+
+      if (upperFruit.includes(upperSearchText)) {
+        const newLiElement = document.createElement("li");
+        const boldFruit = formatFruit(fruit, searchText);
+
+        suggestionsElement.style.padding = "16px";
+        newLiElement.innerHTML = boldFruit;
+        ulElement.append(newLiElement);
+      }
+    });
+  }
+}
+
+// Bold the fruit text based on input - https://stackoverflow.com/a/47676769
+function formatFruit(fruit, searchText) {
+  return fruit.replace(new RegExp(searchText, "gi"), (str) => `<b>${str}</b>`);
+}
+
+// Set the suggestions element to have the same width as the input
+function resizeSuggestions() {
+  suggestionsElement.style.width = inputElement.offsetWidth + "px";
+}
+
+// Clear the suggestions list
+function clearSuggestions() {
+  ulElement.innerHTML = "";
+  suggestionsElement.style.padding = "0";
+}
